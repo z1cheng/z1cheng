43f35781a73b0a14ea70c54832815f8102ca68fd
z1cheng <imchench@gmail.com>
2022-08-20 19:44:20 +0800

    fixup! Add archives of heart rate
    Signed-off-by: z1cheng <imchench@gmail.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-text="My heart rate on $(date +%F) is $1 💖"
+text="My heart rate on $(date +%F) is $1💖"
 sed -i "s/^My heart rate.*/$text/" README.md
 echo "$(date +%F): $1" >>./archives/heart_rate.md
