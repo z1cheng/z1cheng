dd1b8923a7ab6fbacad499802f03899b79d6bf53
z1cheng <imchench@gmail.com>
2022-08-20 23:59:02 +0800

    Not use markdown suffix as archive files
    Signed-off-by: z1cheng <imchench@gmail.com>

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 text="My heart rate on $(date +%F) is $1💖"
 sed -i "s/^My heart rate.*/$text/" README.md
-echo "$(date +%F): $1" >>./archives/heart_rate.md
+echo "$(date +%F): $1" >>./archives/heart_rate
